40bf75172467a8cd81f54b1471a3973f3567b7aa
MD ABDULLAH <MD ABDULLAH>
2024-09-15 18:41:14 +0600

    final commit

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`app is listening on port ${config.port}`);
+      console.log(`app is listening on port server ${config.port}`);
     });
   } catch (err) {
     next(err);
